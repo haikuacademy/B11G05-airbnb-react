c5a184fe4855cc3d144956b41a5e90628caea836
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 12:30:27 +0700

    Modify_Signup

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import SignUp from './components/SignUp'
-import LogIn from './components/LogIn'
+
 import Home from './components/Home'
-import Ale from './components/Ale'
-import House from './components/House'
+
+
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/SignUp' element={<SignUp />} />                <Route path='/' element={<Home />} />
-                <Route path='/ale' element={<Ale />} />
-                <Route path='/house' element={<House />} />
+                <Route path='/SignUp' element={<SignUp />} />
+                <Route path='/' element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
