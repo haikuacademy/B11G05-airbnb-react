4659c27666c79c90783d12455ea9bb3562dc917c
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 14:43:08 +0700

    Updated house.edit layout

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -1,6 +1,6 @@
 function HouseEdit() {
   return (
-    <div className="grid bg-pink-200 gap-8 grid-cols-2 ">
+    <div className="grid bg-pink-200 gap-32 grid-cols-2 ">
       <div className="bg-pink-600">1</div>
       <div className="bg-pink-600">2</div>
     </div>
