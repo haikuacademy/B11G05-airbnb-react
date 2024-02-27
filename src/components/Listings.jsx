dbe7b5ea681d23f9ad09c40bf307f900541f22ac
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-27 14:05:20 +0700

    Grid for listings

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,8 +1,20 @@
 function Listings() {
-    return (
-        <div>
-            <p>This is Listings</p>
-        </div>
-    )
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-28 bg-pink-200">
+        <div className="bg-pink-600">1</div>
+        <div className="bg-pink-600">2</div>
+      </div>
+      <div className="grid gap-4 grid-cols-5 bg-pink-200">
+        <div className="bg-pink-600">1</div>
+        <div className="bg-pink-600">2</div>
+        <div className="bg-pink-600">3</div>
+        <div className="bg-pink-600">4</div>
+        <div className="bg-pink-600">5</div>
+        <div className="bg-pink-600">6</div>
+        <div className="bg-pink-600">7</div>
+      </div>
+    </>
+  )
 }
 export default Listings
