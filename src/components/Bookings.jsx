01f27a0b8044dd2efa8da6523045059535a10462
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 13:13:27 +0700

    Added all components for Airbnb project

---
+++ src/components/Bookings.jsx
@@ -0,0 +1,8 @@
+function Bookings() {
+    return (
+        <div>
+            <p>This is Bookings</p>
+        </div>
+    )
+}
+export default Bookings
---
+++ src/components/HouseEdit.jsx
@@ -0,0 +1,8 @@
+function HouseEdit() {
+    return (
+        <div>
+            <p>Hello from HouseEdit</p>
+        </div>
+    )
+}
+export default HouseEdit
