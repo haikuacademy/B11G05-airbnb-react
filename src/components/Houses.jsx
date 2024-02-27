c7f1b17fe41d21d32a1e2768e4ac4d77982f4334
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 14:27:04 +0700

    Updated houses layout

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -6,6 +6,8 @@ function Houses() {
         <div className="bg-blue-600">2</div>
         <div className="bg-blue-600">3</div>
         <div className="bg-blue-600">4</div>
+        <div className="bg-blue-600">5</div>
+        <div className="bg-blue-600">6</div>
       </div>
       <div className="grid bg-pink-200 gap-2 grid-cols-5 ">
         <div className="bg-pink-600">1</div>
