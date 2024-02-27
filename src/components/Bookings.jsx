ed631f30c76c197b05c7a51a450ed199e8fbf1bb
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-27 13:50:14 +0700

    Bookings with Grid

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,8 +1,14 @@
 function Bookings() {
-    return (
-        <div>
-            <p>This is Bookings</p>
-        </div>
-    )
+  return (
+    <div className="grid gap-4 grid-cols-5 bg-pink-200">
+      <div className="bg-pink-600">1</div>
+      <div className="bg-pink-600">2</div>
+      <div className="bg-pink-600">3</div>
+      <div className="bg-pink-600">4</div>
+      <div className="bg-pink-600">5</div>
+      <div className="bg-pink-600">6</div>
+      <div className="bg-pink-600">7</div>
+    </div>
+  )
 }
 export default Bookings
