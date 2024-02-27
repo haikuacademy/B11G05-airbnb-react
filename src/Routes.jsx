15144775ae387e84507fa9a70baa6350b7c58efb
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 11:51:17 +0700

    Update Route.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import SignUp from './components/SignUp'
+import LogIn from './components/LogIn'
 import Home from './components/Home'
+import Ale from './components/Ale'
+import House from './components/House'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/SignUp' element={<SignUp />} />                <Route path='/' element={<Home />} />
+                <Route path='/ale' element={<Ale />} />
+                <Route path='/house' element={<House />} />
             </Routes>
         </BrowserRouter>
     )
