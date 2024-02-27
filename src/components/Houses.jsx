bd72f38df5c0a81c485acb41fe10db7c02252174
Mooncakeproject <ao.carey@hotmail.com>
2024-02-27 14:37:01 +0700

    Updated houses layout

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,7 +1,7 @@
 function Houses() {
   return (
     <>
-      <div className="flex justify-between bg-blue-200 gap-4 grid-cols-5 ">
+      <div className="flex justify-between bg-blue-200">
         <div className="bg-blue-600">1</div>
         <div className="bg-blue-600">2</div>
         <div className="bg-blue-600">3</div>
@@ -9,7 +9,7 @@ function Houses() {
         <div className="bg-blue-600">5</div>
         <div className="bg-blue-600">6</div>
       </div>
-      <div className="grid bg-pink-200 gap-2 grid-cols-5 ">
+      <div className="grid bg-pink-200 gap-4 grid-cols-5 ">
         <div className="bg-pink-600">1</div>
         <div className="bg-pink-600">2</div>
         <div className="bg-pink-600">3</div>
