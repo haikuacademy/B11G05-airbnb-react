0bcdf3a37ef3ae493fbdb68f7c75af5dad947a3c
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-28 17:00:39 +0700

    Added container to bookings, houseEdit, listing, profile

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -2,13 +2,13 @@ import Nav from './Nav'
 
 function HouseEdit() {
   return (
-    <>
+    <div className="container mx-auto">
       <Nav />
       <div className="grid bg-pink-200 gap-32 grid-cols-2 ">
         <div className="bg-pink-600">1</div>
         <div className="bg-pink-600">2</div>
       </div>
-    </>
+    </div>
   )
 }
 export default HouseEdit
