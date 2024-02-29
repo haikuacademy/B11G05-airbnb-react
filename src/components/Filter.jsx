d102f4fb7cb6b2230987cc5cbf089a8bc8b5dcbe
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-29 12:48:40 +0700

    modified form

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,52 +1,46 @@
 function Filter() {
   return (
-    <div className="flex justify-between rounded-md bg-gray-100 p-2">
+    <div>
       {/* Location */}
-      <form>
+      <form className="flex justify-between rounded-md bg-gray-100 p-2">
         <select className="border border-gray-300 rounded-md p-2 w-60">
           <option selected>Any Location</option>
           <option>Bangkok</option>
           <option>Phuket</option>
           <option>Bali</option>
         </select>
-      </form>
 
-      {/* Rooms */}
-      <select className="border border-gray-300 rounded-md p-2 w-60">
-        <option selected>Any Rooms</option>
-        <option>1 room</option>
-        <option>2 rooms</option>
-        <option>3 rooms</option>
-      </select>
+        {/* Rooms */}
+        <select className="border border-gray-300 rounded-md p-2 w-60">
+          <option selected>Any Rooms</option>
+          <option>1 room</option>
+          <option>2 rooms</option>
+          <option>3 rooms</option>
+        </select>
 
-      {/* Prices */}
-      <form>
+        {/* Prices */}
         <input
           type="number"
           placeholder="max price"
           className="border border-gray-300 rounded-md p-2 w-60"
         />
-      </form>
 
-      {/* Sort by */}
-      <select className="border border-gray-300 rounded-md p-2 w-60">
-        <option selected>sort by</option>
-        <option>location</option>
-        <option>prices</option>
-        <option>rooms</option>
-      </select>
+        {/* Sort by */}
+        <select className="border border-gray-300 rounded-md p-2 w-60">
+          <option selected>sort by</option>
+          <option>location</option>
+          <option>prices</option>
+          <option>rooms</option>
+        </select>
 
-      {/* Keywords */}
-      <form>
+        {/* Keywords */}
         <input
           type="text"
           placeholder="keywords..."
           className="border border-gray-300 rounded-md p-2 w-60"
         />
-      </form>
 
-      {/* button */}
-      <form>
+        {/* button */}
         <button className=" bg-rose-400 text-white py-2 px-4 rounded-md">
           Search
         </button>
