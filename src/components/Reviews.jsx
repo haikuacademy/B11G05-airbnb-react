bf44cdcf85a9475d9b9fd40bc9aba7a147e51896
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-02-29 17:05:46 +0700

    created review form

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -7,7 +7,19 @@ function Reviews() {
       </div>
       <div className="border border-gray-300 rounded-sm m-4 p-4">
         <div>Leave a Review</div>
-        <div className="mt-2">0</div>
+        <div className="mt-2 text-sm">*stars* 0</div>
+        <form>
+          <div className=" text-sm justify-center mt-3">
+            <textarea className=" border w-full col-span-6 text-sm text-gray-300 pb-16 pr-16">
+              Please leave a review for this house...
+            </textarea>
+          </div>
+          <div className=" text-sm text-white mt-1">
+            <button className=" border bg-rose-400 p-2 rounded-md">
+              Submit Review
+            </button>
+          </div>
+        </form>
       </div>
       <Review />
     </div>
