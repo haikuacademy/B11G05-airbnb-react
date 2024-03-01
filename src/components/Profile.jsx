b33b0bad0ce65fa429e384f633ccc44497ab5f6e
Mooncakeproject <ao.carey@hotmail.com>
2024-03-01 11:15:37 +0700

    Update Profile_form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,34 @@ function Profile() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className=" flex justify-start gap-2 bg-blue-200">
-        <div className=" bg-blue-600">1</div>
-        <div className=" bg-blue-600">2</div>
+      <div className="border border-gray-300 p-5">
+        <form>
+          <div className="text-lg font-bold ">Your Profile</div>
+          {/* Create Profile photo & Photo route */}
+          <div className="flex gap-2 ">
+            <img
+              src="https://randomuser.me/api/portraits/men/81.jpg"
+              alt="John Lopez"
+              className="border rounded-full  w-16"
+            />
+            <input
+              type="text"
+              placeholder="https://randomuser.me/api/portraits/men/81.jpg"
+              className="border border-gray-200 w-full "
+            />
+          </div>
+          {/* First name */}
+          <div>First</div>
+          {/* Last name */}
+          <div>Last Name</div>
+          {/* Email */}
+          <div>Email</div>
+          {/* Buttons */}
+          <div>
+            <button>Save Change</button>
+            <button>Log Out</button>
+          </div>
+        </form>
       </div>
     </div>
   )
