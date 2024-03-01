97001f7379a6fb7d6105deae65d80f152515515b
Mooncakeproject <ao.carey@hotmail.com>
2024-03-01 11:45:03 +0700

    Updated profile_form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,32 +4,53 @@ function Profile() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="border border-gray-300 p-5">
+      <div className="border border-gray-300 p-5 rounded-md">
         <form>
           <div className="text-lg font-bold ">Your Profile</div>
           {/* Create Profile photo & Photo route */}
-          <div className="flex gap-2 ">
+          <div className="flex gap-2 mt-4">
             <img
               src="https://randomuser.me/api/portraits/men/81.jpg"
               alt="John Lopez"
               className="border rounded-full  w-16"
             />
-            <input
-              type="text"
-              placeholder="https://randomuser.me/api/portraits/men/81.jpg"
-              className="border border-gray-200 w-full "
-            />
+            <div className="flex justify-center w-full self-center">
+              <input
+                type="text"
+                placeholder="https://randomuser.me/api/portraits/men/81.jpg"
+                className="border border-gray-200 w-full p-2 rounded-md"
+              />
+            </div>
           </div>
           {/* First name */}
-          <div>First</div>
+          <div className="text-gray-400 pt-4">First Name</div>
+          <input
+            type="text"
+            placeholder="John"
+            className="border border-gray-200 w-full p-2 rounded-md placeholder-black "
+          />
           {/* Last name */}
-          <div>Last Name</div>
+          <div className="text-gray-400 pt-2">Last Name</div>
+          <input
+            type="text"
+            placeholder="Lopez"
+            className="border border-gray-200 w-full p-2 rounded-md placeholder-black"
+          />
           {/* Email */}
-          <div>Email</div>
+          <div className="text-gray-400 pt-2">Email</div>
+          <input
+            type="Email"
+            placeholder="john.lopez@gmail.com"
+            className="border border-gray-200 w-full p-2 rounded-md placeholder-black"
+          />
           {/* Buttons */}
-          <div>
-            <button>Save Change</button>
-            <button>Log Out</button>
+          <div className="flex justify-between mt-4">
+            <button className="border rounded bg-rose-400 hover:bg-rose-500 text-white p-2">
+              Save Changes
+            </button>
+            <button className="border rounded bg-white hover:bg-gray-100 text-black py-2 px-4">
+              Logout
+            </button>
           </div>
         </form>
       </div>
