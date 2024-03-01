a0e3970de9b8f1ccffac3b1175c079ef2b134cc6
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-03-01 11:14:40 +0700

    fixed links and hover

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -2,27 +2,27 @@ import { Link } from 'react-router-dom'
 
 function HouseCard() {
   return (
-    <div className="border border-gray-300 rounded-md pb-5">
-      <div className="w-70">
-        <Link to="/houses/1" className="text-rose-400 hover:text-rose-400">
+    <Link to="/houses/1">
+      <div className="border border-gray-300 rounded-md pb-5 hover:text-rose-200">
+        <div className="w-70">
           <img
             src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
-"
+            "
             alt="Phuket house"
             className="w-full rounded-tr-md rounded-tl-md"
           />
-        </Link>
-      </div>
-      <div className=" pl-5 pr-5">
-        <div className=" text-lg font-bold">Phuket, Thailand</div>
-        <div className=" text-gray-500">2 rooms • 2 bathrooms</div>
-        <div className=" text-xl font-bold">$120</div>
-        <div className="flex justify-between mt-3">
-          <span>4.5</span>
-          <span>34</span>
+        </div>
+        <div className=" pl-5 pr-5">
+          <div className=" text-lg font-bold">Phuket, Thailand</div>
+          <div className=" text-gray-500">2 rooms • 2 bathrooms</div>
+          <div className=" text-xl font-bold">$120</div>
+          <div className="flex justify-between mt-3">
+            <span>4.5</span>
+            <span>34</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 export default HouseCard
