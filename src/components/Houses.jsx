da165126179971a8f5d87f5b32845045952a373e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-04 10:20:16 +0700

    created const for map method

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,7 +1,6 @@
 import Nav from './Nav'
 import HouseCard from './HouseCard'
 import Filter from './Filter'
-import Reviews from './Reviews'
 
 function Houses() {
   let houses = [
@@ -16,10 +15,8 @@ function Houses() {
         'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png'
     }
   ]
-
-  //* Use map to render houseData to Reviews  */
-  houses.map((houseInfo, index) => (
-    <Reviews key={index} houseInfo={houseInfo} />
+  const listOfHouseCards = houses.map((house, index) => (
+    <HouseCard key={index} house={house} />
   ))
   return (
     <>
@@ -28,12 +25,9 @@ function Houses() {
         <Filter />
         {/* loop through the houses array and render to HouseCard */}
         <div className="grid gap-4 grid-cols-5 mb-5">
-          {houses.map((house, index) => (
-            <HouseCard key={index} house={house} />
-          ))}
-          {houses.map((house, index) => (
-            <HouseCard key={index} house={house} />
-          ))}
+          {listOfHouseCards}
+          {listOfHouseCards}
+          {listOfHouseCards}
         </div>
       </div>
     </>
