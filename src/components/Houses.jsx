d7d9e8957dae283f36f6f1ffac8a829bd7e0aada
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-06 12:30:05 +0700

    remove hardcoded data

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,30 +1,38 @@
 import Nav from './Nav'
 import HouseCard from './HouseCard'
 import Filter from './Filter'
+import axios from 'axios'
+import { useState } from 'react'
 
 function Houses() {
-  let houses = [
-    {
-      location: 'Phuket, Thailand',
-      rooms: 2,
-      bathrooms: 2,
-      price: 120,
-      rating: 3.5,
-      reviews: 34,
-      photo:
-        'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png'
-    },
-    {
-      location: 'Koh Phangan, Thailand',
-      rooms: 1,
-      bathrooms: 1,
-      price: 100,
-      rating: 4.5,
-      reviews: 17,
-      photo:
-        'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png'
-    }
-  ]
+  const [houses, setHouses] = useState([])
+
+  const getData = async () => {
+    const url = ''
+    const response = await axios.get(url)
+  }
+  // let houses = [
+  //   {
+  //     location: 'Phuket, Thailand',
+  //     rooms: 2,
+  //     bathrooms: 2,
+  //     price: 120,
+  //     rating: 3.5,
+  //     reviews: 34,
+  //     photo:
+  //       'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png'
+  //   },
+  //   {
+  //     location: 'Koh Phangan, Thailand',
+  //     rooms: 1,
+  //     bathrooms: 1,
+  //     price: 100,
+  //     rating: 4.5,
+  //     reviews: 17,
+  //     photo:
+  //       'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png'
+  //   }
+  // ]
   const listOfHouseCards = houses.map((house, index) => (
     <HouseCard key={index} house={house} />
   ))
