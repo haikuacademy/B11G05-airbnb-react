417382882dae5451a5a6bbfe2e709987ead415cf
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-03-06 11:30:16 +0700

    applied useState in gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react'
+
 function Gallery(props) {
+  const [selectedImage, setSelectedImage] = useState(props.images[0])
   return (
     <>
       {/* Left Picture */}
       <div className="grid grid-cols-2 gap-10">
         <div className=" bg-white">
-          <img src={props.images[0]} alt="Sample" className=" rounded-md" />
+          <img src={selectedImage} alt="Sample" className=" rounded-md" />
         </div>
 
         {/* Right Pictures */}
         <div className="grid grid-cols-3 gap-2">
           {props.images.map((image, index) => (
             <div key={index} className=" bg-white">
-              <img src={image} alt="Sample" className=" rounded-md" />
+              <img
+                src={image}
+                alt="Sample"
+                className=" rounded-md"
+                onClick={(event) => {
+                  setSelectedImage(image)
+                }}
+              />
             </div>
           ))}
         </div>
