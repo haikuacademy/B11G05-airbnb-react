325b28e766aea45868e8537d32ab9c06e872ad9b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-08 22:49:25 +0700

    pass the props to filter

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -18,28 +18,6 @@ function Houses() {
     getHouses()
   }, [])
 
-  // let houses = [
-  //   {
-  //     location: 'Phuket, Thailand',
-  //     rooms: 2,
-  //     bathrooms: 2,
-  //     price: 120,
-  //     rating: 3.5,
-  //     reviews: 34,
-  //     photo:
-  //       'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png'
-  //   },
-  //   {
-  //     location: 'Koh Phangan, Thailand',
-  //     rooms: 1,
-  //     bathrooms: 1,
-  //     price: 100,
-  //     rating: 4.5,
-  //     reviews: 17,
-  //     photo:
-  //       'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png'
-  //   }
-  // ]
   const listOfHouseCards = houses.map((house, index) => (
     <HouseCard key={index} house={house} />
   ))
@@ -47,7 +25,8 @@ function Houses() {
     <>
       <div className=" container mx-auto">
         <Nav />
-        <Filter />
+        {/* pass the setHouses function to the Filters component as a prop */}
+        <Filter setHouses={setHouses} />
         {/* loop through the houses array and render to HouseCard */}
         <div className="grid gap-4 grid-cols-5 mb-5">{listOfHouseCards}</div>
       </div>
