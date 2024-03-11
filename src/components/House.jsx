db9bb63743f663277540eb641598f2fa174a3524
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-11 18:18:29 +0700

    move booking component

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -5,6 +5,7 @@ import Reviews from './Reviews'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import Bookings from './Bookings'
 
 function House() {
   //useParams hook to extract the id  from params.id of the house from the URL
@@ -22,7 +23,6 @@ function House() {
     )
     console.log('data------->', data)
     setHouse(data)
-    console.log('house---->', house)
     setLoading(false)
   }
   // Use the useEffect hook to trigger the getHouse function when the component loads
@@ -30,36 +30,10 @@ function House() {
     getHouse()
   }, [])
 
-  //creating variables for getting booking date
-  const [startDate, setStartDate] = useState('')
-  const [endDate, setEndDate] = useState('')
-  const [nights, setNights] = useState(0)
-  const [totalPrice, setTotalPrice] = useState(0)
-
   useEffect(() => {
-    if (startDate && endDate) {
-      const start = new Date(startDate)
-      const end = new Date(endDate)
-      const calculateTime = end.getTime() - start.getTime()
-      const nights = Math.ceil(calculateTime / (1000 * 3600 * 24))
-      setNights(nights || 0)
-    }
-  }, [startDate, endDate])
+    console.log('house---->', house)
+  }, [loading])
 
-  // useEffect to update totalPrice
-  useEffect(() => {
-    setTotalPrice(nights * house.price)
-  }, [nights, house.price])
-
-  // Function to get check-in date from the form
-  const getCheckInDate = (e) => {
-    setStartDate(e.target.value)
-  }
-
-  // Function to get check-out date from the form
-  const getCheckOutDate = (e) => {
-    setEndDate(e.target.value)
-  }
   return (
     <div className="container mx-auto">
       <Nav />
@@ -97,57 +71,8 @@ function House() {
               </div>
               <div className="my-8 text-sm">{house.description}</div>
             </div>
-
-            {/* booking form */}
-            <div className="border border-gray-200 self-start my-5 ml-5 p-4 rounded-md">
-              <form>
-                <span className="flex text-xl font-bold">
-                  <div>$</div>
-                  <div>{house.price}</div>
-                </span>
-                <span className=" text-gray-400 font-bold text-sm">
-                  {' '}
-                  /night
-                </span>
-                <div className="flex justify-between mt-5 gap-1">
-                  {/* checkin date */}
-                  <div className="w-full">
-                    <div className=" text-xs text-gray-400">Check-in</div>
-                    <input
-                      type="date"
-                      className="border border-gray-200 rounded-md p-2 w-full"
-                      value={startDate}
-                      onChange={getCheckInDate}
-                    />
-                  </div>
-                  {/* checkout date */}
-                  <div className="w-full">
-                    <div className=" text-xs text-gray-400">Check-out</div>
-                    <input
-                      type="date"
-                      className="border border-gray-200 rounded-md p-2 w-full"
-                      value={endDate}
-                      onChange={getCheckOutDate}
-                    />
-                  </div>
-                </div>
-                <textarea
-                  placeholder="Please send a message to the host..."
-                  className="border border-gray-200 rounded-md mt-2 w-full p-2"
-                  rows="7"
-                ></textarea>
-                <div className="flex justify-between">
-                  {/* total nights */}
-                  <div className="flex self-center">
-                    <span>{nights} nights = </span>
-                    <span className="font-bold">{`$ ${totalPrice}`}</span>
-                  </div>
-                  <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
-                    Reserve
-                  </button>
-                </div>
-              </form>
-            </div>
+            {/* Use the Booking component and pass the house prop */}
+            <Bookings house={house} />
           </div>
           <Reviews house={house} />
         </>
