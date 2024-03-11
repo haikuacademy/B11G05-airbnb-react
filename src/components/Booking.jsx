2b14c4d021f2802b0e98ee59b57e15d0ad2bd1c1
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-11 18:27:15 +0700

    remover ternary operator

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -6,7 +6,7 @@ function Booking({ house }) {
   const [endDate, setEndDate] = useState('')
   const [nights, setNights] = useState(0)
   const [totalPrice, setTotalPrice] = useState(0)
-  console.log(house)
+
   useEffect(() => {
     if (startDate && endDate) {
       const start = new Date(startDate)
@@ -38,56 +38,52 @@ function Booking({ house }) {
   return (
     <>
       {/* booking form */}
-      {house ? (
-        <div className="border border-gray-200 self-start my-5 ml-5 p-4 rounded-md">
-          <form>
-            <span className="flex text-xl font-bold">
-              <div>$</div>
-              <div>{house.price}</div>
-            </span>
-            <span className=" text-gray-400 font-bold text-sm"> /night</span>
-            <div className="flex justify-between mt-5 gap-1">
-              {/* checkin date */}
-              <div className="w-full">
-                <div className=" text-xs text-gray-400">Check-in</div>
-                <input
-                  type="date"
-                  className="border border-gray-200 rounded-md p-2 w-full"
-                  value={startDate}
-                  onChange={getCheckInDate}
-                />
-              </div>
-              {/* checkout date */}
-              <div className="w-full">
-                <div className=" text-xs text-gray-400">Check-out</div>
-                <input
-                  type="date"
-                  className="border border-gray-200 rounded-md p-2 w-full"
-                  value={endDate}
-                  onChange={getCheckOutDate}
-                />
-              </div>
+      <div className="border border-gray-200 self-start my-5 ml-5 p-4 rounded-md">
+        <form>
+          <span className="flex text-xl font-bold">
+            <div>$</div>
+            <div>{house.price}</div>
+          </span>
+          <span className=" text-gray-400 font-bold text-sm"> /night</span>
+          <div className="flex justify-between mt-5 gap-1">
+            {/* checkin date */}
+            <div className="w-full">
+              <div className=" text-xs text-gray-400">Check-in</div>
+              <input
+                type="date"
+                className="border border-gray-200 rounded-md p-2 w-full"
+                value={startDate}
+                onChange={getCheckInDate}
+              />
             </div>
-            <textarea
-              placeholder="Please send a message to the host..."
-              className="border border-gray-200 rounded-md mt-2 w-full p-2"
-              rows="7"
-            ></textarea>
-            <div className="flex justify-between">
-              {/* total nights */}
-              <div className="flex self-center">
-                <span>{nights} nights = </span>
-                <span className="font-bold">{`$ ${totalPrice}`}</span>
-              </div>
-              <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
-                Reserve
-              </button>
+            {/* checkout date */}
+            <div className="w-full">
+              <div className=" text-xs text-gray-400">Check-out</div>
+              <input
+                type="date"
+                className="border border-gray-200 rounded-md p-2 w-full"
+                value={endDate}
+                onChange={getCheckOutDate}
+              />
             </div>
-          </form>
-        </div>
-      ) : (
-        <div>Loading</div>
-      )}
+          </div>
+          <textarea
+            placeholder="Please send a message to the host..."
+            className="border border-gray-200 rounded-md mt-2 w-full p-2"
+            rows="7"
+          ></textarea>
+          <div className="flex justify-between">
+            {/* total nights */}
+            <div className="flex self-center">
+              <span>{nights} nights = </span>
+              <span className="font-bold">{`$ ${totalPrice}`}</span>
+            </div>
+            <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
+              Reserve
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
