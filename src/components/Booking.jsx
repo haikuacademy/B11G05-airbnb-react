2bd8a53ea2a78e21db4d876850e62fc95e854cb6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-11 20:00:13 +0700

    add thank you message

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -8,6 +8,7 @@ function Booking({ house }) {
   const [endDate, setEndDate] = useState('')
   const [nights, setNights] = useState(0)
   const [totalPrice, setTotalPrice] = useState(0)
+  const [booked, hasBooked] = useState(false)
 
   // Function to get check-in date from the form
   const getCheckInDate = (e) => {
@@ -35,6 +36,8 @@ function Booking({ house }) {
       formObject
     )
     console.log('response', response.data)
+    //check if the review is posted
+    hasBooked(true)
   }
 
   useEffect(() => {
@@ -56,55 +59,61 @@ function Booking({ house }) {
 
   return (
     <>
-      {/* booking form */}
       <div className="border border-gray-200 self-start my-5 ml-5 p-4 rounded-md">
-        <form onSubmit={createBooking}>
-          <span className="flex text-xl font-bold">
-            <div>$</div>
-            <div>{house.price}</div>
-          </span>
-          <span className=" text-gray-400 font-bold text-sm"> /night</span>
-          <div className="flex justify-between mt-5 gap-1">
-            {/* checkin date */}
-            <div className="w-full">
-              <div className=" text-xs text-gray-400">Check-in</div>
-              <input
-                name="from_date"
-                type="date"
-                className="border border-gray-200 rounded-md p-2 w-full"
-                value={startDate}
-                onChange={getCheckInDate}
-              />
-            </div>
-            {/* checkout date */}
-            <div className="w-full">
-              <div className=" text-xs text-gray-400">Check-out</div>
-              <input
-                name="to_date"
-                type="date"
-                className="border border-gray-200 rounded-md p-2 w-full"
-                value={endDate}
-                onChange={getCheckOutDate}
-              />
-            </div>
+        {/* booking form */}
+        {booked ? (
+          <div className=" bg-emerald-50 text-center rounded p-6 mt-3 h-20">
+            Thank you for your reservation!
           </div>
-          <textarea
-            name="message"
-            placeholder="Please send a message to the host..."
-            className="border border-gray-200 rounded-md mt-2 w-full p-2"
-            rows="7"
-          ></textarea>
-          <div className="flex justify-between">
-            {/* total nights */}
-            <div className="flex self-center">
-              <span>{nights} nights = </span>
-              <span className="font-bold">{`$ ${totalPrice}`}</span>
+        ) : (
+          <form onSubmit={createBooking}>
+            <span className="flex text-xl font-bold">
+              <div>$</div>
+              <div>{house.price}</div>
+            </span>
+            <span className=" text-gray-400 font-bold text-sm"> /night</span>
+            <div className="flex justify-between mt-5 gap-1">
+              {/* checkin date */}
+              <div className="w-full">
+                <div className=" text-xs text-gray-400">Check-in</div>
+                <input
+                  name="from_date"
+                  type="date"
+                  className="border border-gray-200 rounded-md p-2 w-full"
+                  value={startDate}
+                  onChange={getCheckInDate}
+                />
+              </div>
+              {/* checkout date */}
+              <div className="w-full">
+                <div className=" text-xs text-gray-400">Check-out</div>
+                <input
+                  name="to_date"
+                  type="date"
+                  className="border border-gray-200 rounded-md p-2 w-full"
+                  value={endDate}
+                  onChange={getCheckOutDate}
+                />
+              </div>
             </div>
-            <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
-              Reserve
-            </button>
-          </div>
-        </form>
+            <textarea
+              name="message"
+              placeholder="Please send a message to the host..."
+              className="border border-gray-200 rounded-md mt-2 w-full p-2"
+              rows="7"
+            ></textarea>
+            <div className="flex justify-between">
+              {/* total nights */}
+              <div className="flex self-center">
+                <span>{nights} nights = </span>
+                <span className="font-bold">{`$ ${totalPrice}`}</span>
+              </div>
+              <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
+                Reserve
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </>
   )
