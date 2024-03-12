747d1d293a49cc53d29cf93e535ba45c2f182712
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-12 12:44:21 +0700

    added create booking function

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,10 +1,55 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function Booking({ house }) {
+  console.log(house)
   //creating variables for getting booking date
   const [startDate, setStartDate] = useState('')
   const [endDate, setEndDate] = useState('')
   const [nights, setNights] = useState(0)
   const [totalPrice, setTotalPrice] = useState(0)
+  const [booked, hasBooked] = useState(false)
+  const [error, setError] = useState('')
+
+  // Function to get check-in date from the form
+  const getCheckInDate = (e) => {
+    setStartDate(e.target.value)
+  }
+
+  // Function to get check-out date from the form
+  const getCheckOutDate = (e) => {
+    setEndDate(e.target.value)
+  }
+
+  // Function to create booking
+  const createBooking = async (e) => {
+    try {
+      //prevent browser from reload
+      e.preventDefault()
+      //get data from form from e.target
+      const form = new FormData(e.target)
+      let formObject = Object.fromEntries(form.entries())
+      console.log('formObject', formObject)
+      //get house id from the props
+      formObject.house_id = house.house_id
+      console.log('formObject with house_id', formObject)
+      let response = await axios.post(
+        'https://haiku-bnb.onrender.com/bookings',
+        formObject
+      )
+      console.log('response', response.data)
+      //check if there's an error with booking date
+      if (response.data.error) {
+        setError(response.data.error)
+        hasBooked(false)
+      } else {
+        //check if the review is posted
+        hasBooked(true)
+      }
+    } catch (error) {
+      console.error('An error occurred:', error.message)
+    }
+  }
   useEffect(() => {
     if (startDate && endDate) {
       const start = new Date(startDate)
@@ -20,65 +65,70 @@ function Booking({ house }) {
     if (house) {
       setTotalPrice(nights * house.price)
     }
+    // eslint-disable-next-line
   }, [nights])
 
-  // Function to get check-in date from the form
-  const getCheckInDate = (e) => {
-    setStartDate(e.target.value)
-  }
-
-  // Function to get check-out date from the form
-  const getCheckOutDate = (e) => {
-    setEndDate(e.target.value)
-  }
   return (
     <>
-      {/* booking form */}
       <div className="border border-gray-200 self-start my-5 ml-5 p-4 rounded-md">
-        <form>
-          <span className="flex text-xl font-bold">
-            <div>$</div>
-            <div>{house.price}</div>
-          </span>
-          <span className=" text-gray-400 font-bold text-sm"> /night</span>
-          <div className="flex justify-between mt-5 gap-1">
-            {/* checkin date */}
-            <div className="w-full">
-              <div className=" text-xs text-gray-400">Check-in</div>
-              <input
-                type="date"
-                className="border border-gray-200 rounded-md p-2 w-full"
-                value={startDate}
-                onChange={getCheckInDate}
-              />
-            </div>
-            {/* checkout date */}
-            <div className="w-full">
-              <div className=" text-xs text-gray-400">Check-out</div>
-              <input
-                type="date"
-                className="border border-gray-200 rounded-md p-2 w-full"
-                value={endDate}
-                onChange={getCheckOutDate}
-              />
-            </div>
+        {/* booking form */}
+        {booked ? (
+          <div className=" bg-emerald-50 text-center rounded p-6 mt-3 h-20">
+            Thank you for your reservation!
           </div>
-          <textarea
-            placeholder="Please send a message to the host..."
-            className="border border-gray-200 rounded-md mt-2 w-full p-2"
-            rows="7"
-          ></textarea>
-          <div className="flex justify-between">
-            {/* total nights */}
-            <div className="flex self-center">
-              <span>{nights} nights = </span>
-              <span className="font-bold">{`$ ${totalPrice}`}</span>
+        ) : (
+          <form onSubmit={createBooking}>
+            <span className="flex text-xl font-bold">
+              <div>$</div>
+              <div>{house.price}</div>
+            </span>
+            <span className=" text-gray-400 font-bold text-sm"> /night</span>
+            <div className="flex justify-between mt-5 gap-1">
+              {/* checkin date */}
+              <div className="w-full">
+                <div className=" text-xs text-gray-400">Check-in</div>
+                <input
+                  name="from_date"
+                  type="date"
+                  className="border border-gray-200 rounded-md p-2 w-full"
+                  value={startDate}
+                  onChange={getCheckInDate}
+                />
+              </div>
+              {/* checkout date */}
+              <div className="w-full">
+                <div className=" text-xs text-gray-400">Check-out</div>
+                <input
+                  name="to_date"
+                  type="date"
+                  className="border border-gray-200 rounded-md p-2 w-full"
+                  value={endDate}
+                  onChange={getCheckOutDate}
+                />
+              </div>
             </div>
-            <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
-              Reserve
-            </button>
-          </div>
-        </form>
+            <textarea
+              name="message"
+              placeholder="Please send a message to the host..."
+              className="border border-gray-200 rounded-md mt-2 w-full p-2"
+              rows="7"
+            ></textarea>
+            <div className="flex justify-between">
+              {/* total nights */}
+              <div className="flex self-center">
+                <span>{nights} nights = </span>
+                <span className="font-bold">{`$ ${totalPrice}`}</span>
+              </div>
+              <button className=" bg-rose-400 text-white py-3 px-4 rounded-md">
+                Reserve
+              </button>
+            </div>
+          </form>
+        )}
+        {/* add error message */}
+        {error ? (
+          <span className=" text-red-500 text-xs pl-1">{error}</span>
+        ) : null}
       </div>
     </>
   )
