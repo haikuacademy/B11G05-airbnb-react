09e2c1a453f4174489d6867a03d62488486d51f9
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-12 15:03:13 +0700

    added get bookings

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,37 +1,26 @@
 import Nav from './Nav'
 import HouseCard from './HouseCard'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Bookings() {
-  let bookings = [
-    {
-      location: 'Bangkok, Thailand',
-      rooms: 3,
-      bathrooms: 2,
-      price: 56,
-      rating: 4.5,
-      reviews: 17,
-      photo:
-        'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png',
-      startDate: '2 Jan 2024',
-      endDate: '5 Jan 2024',
-      totalNights: 3,
-      totalPrice: '$168'
-    },
-    {
-      location: 'Koh Samui, Thailand',
-      rooms: 1,
-      bathrooms: 1,
-      price: 100,
-      rating: 4.5,
-      reviews: 17,
-      photo:
-        'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png',
-      startDate: '7 Aug 2024',
-      endDate: '14 Aug 2024',
-      totalNights: 7,
-      totalPrice: '$1,330'
+  const [bookings, setBookings] = useState([])
+
+  // function to get bookings
+  const getBookings = async (e) => {
+    try {
+      let response = await axios.get('https://haiku-bnb.onrender.com/bookings')
+      console.log('response data', response.data)
+      setBookings(response.data)
+    } catch (error) {
+      console.error(error.message)
     }
-  ]
+  }
+  // Use the useEffect hook to trigger the getBookings function when the component loads
+  useEffect(() => {
+    getBookings()
+  }, [])
+
   const listOfBookings = bookings.map((booking, index) => (
     <HouseCard key={index} house={booking} isBooking={true} />
   ))
