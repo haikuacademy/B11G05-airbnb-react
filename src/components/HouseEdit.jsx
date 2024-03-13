26e120f08211356186e0e5675f4381f386dff12f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-03-13 16:55:02 +0700

    added patch request

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -1,48 +1,57 @@
 import Nav from './Nav'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function HouseEdit() {
-  const isLoggedIn = localStorage.getItem('isLoggedIn')
-  const house = {
-    location: 'Phuket, Thailand',
-    rooms: 2,
-    bathrooms: 2,
-    description: `Nestled on a serene beachfront, this charming Airbnb house offers a
-            picturesque escape. The exterior boasts a classic beach house
-            aesthetic with weathered wood siding and a spacious wraparound deck,
-            perfect for savoring the ocean breeze. Inside, you're greeted by an
-            open-concept living area bathed in natural light, complemented by
-            cozy furnishings and nautical accents. The house features three
-            comfortable bedrooms, each with a unique coastal theme, and two
-            modern bathrooms. The fully equipped kitchen opens to a dining area
-            that's ideal for intimate meals or entertaining guests. Large glass
-            doors in the living room lead to the deck, where you can enjoy
-            stunning sunset views over the ocean. This idyllic retreat is a
-            stone's throw away from the soft sandy beach, making it the perfect
-            spot for beach lovers and those seeking a tranquil getaway.`,
-    price: 120,
-    rating: 4.5,
-    host: {
-      firstName: 'Linda',
-      lastName: 'Smith',
-      picture: 'https://randomuser.me/api/portraits/women/85.jpg'
-    },
-    images: [
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_02.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_05.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_07.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_08.png',
-      'https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295019/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_09.png'
-    ]
+  //params
+  const params = useParams()
+  // declare useNavigate
+  const navigate = useNavigate()
+
+  const [house, setHouse] = useState({
+    images: []
+  })
+  //Create an async function getHouse that uses axios to get a house object from the API url /houses/1
+  // then sets the object as the value of the house state variable
+  const getHouse = async () => {
+    let { data } = await axios.get(
+      `https://haiku-bnb.onrender.com/houses/${params.id}`
+    )
+    setHouse(data)
   }
+  // Use the useEffect hook to trigger the getHouse function when the component loads
+  useEffect(() => {
+    getHouse()
+  }, [])
+
+  //update the data with patch
+  const updateHouse = async (e) => {
+    //1. prevent browser from reload
+    try {
+      e.preventDefault()
+      //2 get data from form from e.target
+      const form = new FormData(e.target)
+      const formObj = Object.fromEntries(form.entries())
+      //retrieves all 'images' field in an array of strings
+      formObj.photos = form.getAll('images')
+      formObj.house_id = params
+      let response = await axios.patch(
+        `https://haiku-bnb.onrender.com/houses/${params.id}`,
+        formObj
+      )
+      setHouse(response.data)
+      navigate(`/houses/${params.id}`)
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
   return (
     <div className="container mx-auto">
       <Nav />
-      <form>
+      <form onSubmit={updateHouse}>
         <div className="border border-gray-200 p-2 rounded-md mt-2">
           <div className="grid gap-32 grid-cols-2 ">
             {/* col 1 */}
@@ -50,82 +59,109 @@ function HouseEdit() {
               <div>Edit your listing</div>
               <div className=" text-gray-400 mt-2 text-sm">Location</div>
               <input
+                defaultValue={house.location}
+                name="location"
                 type="text"
                 placeholder={house.location}
                 className="border border-gray-200 rounded-md p-2 w-full placeholder-black"
               />
               <div className=" text-gray-400 mt-2 text-sm">Bedrooms</div>
               <input
+                defaultValue={house.rooms}
+                name="rooms"
                 type="number"
                 placeholder={house.rooms}
                 className="border border-gray-200 rounded-md p-2 w-full placeholder-black"
               />
               <div className=" text-gray-400 mt-2 text-sm">Bathrooms</div>
               <input
+                defaultValue={house.bathrooms}
+                name="bathrooms"
                 type="number"
                 placeholder={house.bathrooms}
                 className="border border-gray-200 rounded-md p-2 w-full placeholder-black"
               />
               <div className=" text-gray-400 mt-2 text-sm">Price per Night</div>
               <input
+                defaultValue={house.price}
+                name="price"
                 type="number"
                 placeholder={house.price}
                 className="border border-gray-200 rounded-md p-2 w-full placeholder-black"
               />
               <div className=" text-gray-400 mt-2 text-sm">Description</div>
               <textarea
+                defaultValue={house.description}
+                name="description"
                 className="border border-gray-200 rounded-md p-2 w-full text-sm"
                 rows="4"
-              >
-                {house.description}
-              </textarea>
+                placeholder={house.description}
+              ></textarea>
             </div>
             {/* col 2 */}
             <div>
               <div className=" text-gray-400 mt-2 text-sm">Photos</div>
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[0]}
+                defaultValue={house.images && house.images[0]}
+                placeholder={house.images && house.images[0]}
                 className="border border-gray-200 rounded-md p-2 w-full placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[1]}
+                defaultValue={house.images && house.images[1]}
+                placeholder={house.images && house.images[1]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[2]}
+                defaultValue={house.images && house.images[2]}
+                placeholder={house.images && house.images[2]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[3]}
+                defaultValue={house.images && house.images[3]}
+                placeholder={house.images && house.images[3]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[4]}
+                defaultValue={house.images && house.images[4]}
+                placeholder={house.images && house.images[4]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[5]}
+                defaultValue={house.images && house.images[5]}
+                placeholder={house.images && house.images[5]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[6]}
+                defaultValue={house.images && house.images[6]}
+                placeholder={house.images && house.images[6]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[7]}
+                defaultValue={house.images && house.images[7]}
+                placeholder={house.images && house.images[7]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
               <input
+                name="images"
                 type="text"
-                placeholder={house.images[8]}
+                defaultValue={house.images && house.images[8]}
+                placeholder={house.images && house.images[8]}
                 className="border border-gray-200 rounded-md p-2 w-full mt-2 placeholder-black"
               />
             </div>
@@ -147,11 +183,3 @@ function HouseEdit() {
   )
 }
 export default HouseEdit
-
-//  {isLoggedIn ? (
-//           <>
-//             <Link to="/bookings">
-//               <button className="border border-gray-300 rounded-md p-2">
-//                 My Bookings
-//               </button>
-//             </Link>
